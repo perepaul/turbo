60fecd3b55bcc9c2995a9f45cb33b61bae44065b
Perekpo Emmanuel <perepaulonline@gmail.com>
2021-08-19 20:22:24 +0100

    breadcrumb removed

--- resources/views/admin/emails/send.blade.php
+++ resources/views/admin/emails/send.blade.php
@@ -3,13 +3,6 @@
 @section('content')
     <div class="container-fluid">
 
-        <div class="row page-titles">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">Email</a></li>
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Compose</a></li>
-            </ol>
-        </div>
-
         <!-- row -->
         <div class="row">
             <div class="col-lg-12">
