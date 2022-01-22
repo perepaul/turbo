f85bce9570c37ebab60b598a8035c1a69ff35cda
= <=>
2022-01-22 12:04:42 +0100

    whatsapp button and tidio

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -134,6 +134,9 @@
                 </div>
             </div>
         </footer>
+        <a href="https://api.whatsapp.com/send?phone={{$contact?->phone}}" class="whatsapp-button-wrapper" target="_blank">
+            <i class="fa fa-whatsapp"></i>
+        </a>
         <!-- ##### Footer Area End ##### -->
 
         <!-- ########## All JS ########## -->
