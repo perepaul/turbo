87ce53b685ff15cac8180ea1d511bc150110d8f1
= <=>
2022-03-04 14:43:41 +0100

    changed route to request

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -184,7 +184,7 @@
                                         <img src="{{profile_picture()}}" width="20" alt="" />
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-end">
-                                        @if(route()->isUser())
+                                        @if(request()->isUser())
                                         <a href="{{route('user.profile')}}" class="dropdown-item ai-icon">
                                             <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                 <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
