35e0ef10ec4ee9ba6adbfaf98aa18d57be257b54
= <=>
2022-03-21 17:25:01 +0100

    typo correction

--- resources/views/emails/withdrawal-accepted.blade.php
+++ resources/views/emails/withdrawal-accepted.blade.php
@@ -1,9 +1,8 @@
 @component('mail::message')
 # Dear {{$withdrawal->user->firstname}}
 
-The withdrawal request of {{format_mone($withdrawal->amount)}} has been accepted.
+The withdrawal request of {{format_money($withdrawal->amount)}} has been accepted.
 
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
-
