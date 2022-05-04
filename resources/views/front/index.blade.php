f68360d8fd591bf530927bcb79b39a78c38605f0
= <=>
2022-05-04 23:22:10 +0100

    modifications

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -35,15 +35,15 @@
                     </div>
                 </div>
                 <!-- Welcome Video Area -->
-                <div class="col-12 col-lg-6 col-md-12">
+                {{-- <div class="col-12 col-lg-6 col-md-12">
                     <div class="main-ilustration wow fadeInUp" data-wow-delay="0.5s">
-
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
 </section>
+<x-trade.marquee />
 <!-- ##### Welcome Area End ##### -->
 
 {{-- <div class="vertical-social">
