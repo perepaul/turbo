d40b14e4467735d2d3d8d956dfebe763ac5b7e07
= <=>
2022-05-10 03:55:45 +0100

    changeed first to Name

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -209,7 +209,7 @@
                                     <h4 class="text-primary mb-4">Personal Information</h4>
                                     <div class="row mb-2">
                                         <div class="col-sm-3 col-5">
-                                            <h5 class="f-w-500">First <span class="pull-end">:</span>
+                                            <h5 class="f-w-500">Name <span class="pull-end">:</span>
                                             </h5>
                                         </div>
                                         <div class="col-sm-9 col-7"><span>{{$user->name}}</span>
