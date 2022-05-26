83ca65575285096bb2222a135fe90ae44c025390
= <=>
2022-05-26 20:31:41 +0100

    pending done - moving to admin

--- resources/views/user/regional-rep.blade.php
+++ resources/views/user/regional-rep.blade.php
@@ -8,18 +8,18 @@
                 @csrf
                 <div class="form-group col-md-6">
                     <label for="firstname">First name</label>
-                    <input id="firstname" type="text" class="form-control" name="firstname" value="{{$rep?->firstname}}">
+                    <input id="firstname" type="text" class="form-control" name="firstname" value="{{ $rep?->firstname ?? old('firstname') }}">
                     <x-error key="firstname" />
                 </div>
                 <div class="form-group col-md-6">
                     <label for="lastname">Last name</label>
-                    <input id="lastname" type="text" class="form-control" name="lastname" value="{{$rep?->lastname}}">
+                    <input id="lastname" type="text" class="form-control" name="lastname" value="{{ $rep?->lastname ?? old('lastname') }}">
                     <x-error key="lastname" />
                 </div>
                 <div class="form-group col-md-6">
                     <label for="">Country</label>
                     <select name="country" id="country" class="form-select">
-                        <x-countries country="{{$rep?->country}}" />
+                        <x-countries country="{{$rep?->country ?? old('country')}}" />
                     </select>
                     <x-error key="country" />
                 </div>
@@ -27,35 +27,35 @@
                 <div class="mb-3 col-md-6">
                     <label class="form-label">State/Region</label>
                     <select name="state" id="state" class="form-select">
-                        <x-states selected="{{$rep?->state}}" country="{{$rep?->country}}" />
+                        <x-states selected="{{$rep?->state ?? old('state') }}" country="{{$rep?->country ?? old('country') }}" />
                     </select>
                     <x-error key="state" />
                 </div>
 
                 <div class="form-group">
                     <label for="address">Address</label>
-                    <input id="address" type="text" class="form-control" name="address" value="{{$rep?->address}}">
+                    <input id="address" type="text" class="form-control" name="address" value="{{$rep?->address ?? old('address') }}">
                     <x-error key="address" />
                 </div>
 
                 <div class="form-group col-md-8">
                     <label for="city">City</label>
-                    <input id="city" type="text" class="form-control" name="city" value="{{$rep?->city}}">
+                    <input id="city" type="text" class="form-control" name="city" value="{{$rep?->city ?? old('city') }}">
                     <x-error key="city" />
                 </div>
                 <div class="form-group col-md-4 mb-3">
                     <label for="zip_code">Zip/Postal Code</label>
-                    <input id="zip_code" type="text" class="form-control" name="zip_code" value="{{$rep?->zip_code}}">
+                    <input id="zip_code" type="text" class="form-control" name="zip_code" value="{{$rep?->zip_code ?? old('zip_code') }}">
                     <x-error key="zip_code" />
                 </div>
                 <div class="col-12 mb-3">
                     <p>Would you love to share our platform on your social media account?</p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="question_one" id="question_one_yes" value="yes" @if($rep?->question_one == 'yes') checked="checked" @endif>
+                        <input class="form-check-input" type="radio" name="question_one" id="question_one_yes" value="yes" @if($rep?->question_one == 'yes' ?? old('question_one') == 'yes') checked="checked" @endif>
                         <label class="form-check-label" for="question_one_yes">Yes</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="question_one" id="question_one_no" value="no" @if($rep?->question_one == 'no') checked="checked" @endif>
+                        <input class="form-check-input" type="radio" name="question_one" id="question_one_no" value="no" @if($rep?->question_one == 'no' ?? old('question_one') == 'no') checked="checked" @endif>
                         <label class="form-check-label" for="question_one_no">No</label>
                     </div>
                     <x-error key="question_one" />
@@ -64,11 +64,11 @@
                 <div class="col-12 mb-3">
                     <p>Would you do promotional materials on behalf of the company for marketing purposes?</p>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="question_two" id="question_two_yes" value="yes" @if($rep?->question_two == 'yes') checked="checked" @endif>
+                        <input class="form-check-input" type="radio" name="question_two" id="question_two_yes" value="yes" @if($rep?->question_two == 'yes' ?? old('question_two') == 'yes') checked="checked" @endif>
                         <label class="form-check-label" for="question_two_yes">Yes</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="question_two" id="question_two_no" value="no" @if($rep?->question_two == 'no') checked="checked" @endif>
+                        <input class="form-check-input" type="radio" name="question_two" id="question_two_no" value="no" @if($rep?->question_two == 'no' ?? old('question_two') == 'no') checked="checked" @endif>
                         <label class="form-check-label" for="question_two_no">No</label>
                     </div>
                     <x-error key="question_two" />
@@ -76,7 +76,7 @@
 
                 <div class="form-group">
                     <label for="question_three">To what extent do you intent to promote the platform?</label>
-                    <textarea style="min-height: 100px;" name="question_three" id="question_three" class="form-control" placeholder="To what extent do you intent to promote the platform?">{{$rep?->question_three}}</textarea>
+                    <textarea style="min-height: 100px;" name="question_three" id="question_three" class="form-control" placeholder="To what extent do you intent to promote the platform?">{{$rep?->question_three ?? old('question_three')}}</textarea>
                     <x-error key="question_three" />
                 </div>
 
@@ -112,6 +112,12 @@
 @endsection
 
 @push('js')
+
+@if (in_array($rep?->status,['pending','approved']))
+<script>
+    $('input,textarea,select').attr('disabled',true).attr('readonly',true);
+</script>
+@endif
 <script>
     $(document).on('change','#passport', function(e){
         let file = $(e.currentTarget).prop('files')[0];
