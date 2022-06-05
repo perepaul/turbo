201866b4b9c9b811acba0ebe4ef45333f048cc60
= <=>
2022-06-05 22:19:23 +0100

    method errors correction

--- resources/views/admin/method/index.blade.php
+++ resources/views/admin/method/index.blade.php
@@ -2,53 +2,51 @@
 @section('title', 'Payment methods')
 @section('content')
 <div class="row">
-    <div class="col-md-8 mx-auto">
+    <div class="col-md-10 mx-auto">
         <x-alert />
         <div class="card">
             <div class="card-body">
-                <div class="p-lg-5">
-                    <div class="text-end">
-                        <a href="{{ route('admin.settings.methods.create') }}" class="btn btn-outline-success btn-sm mb-3"><i class="fa fa-plus"></i> Add Method</a>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Address</th>
-                                    <th>Image</th>
-                                    <th>Status</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($methods as $method)
-                                <tr>
-                                    <td>{{ $method->name }}</td>
-                                    <td>{{ $method->address }}</td>
-                                    <td>@if($method->image)<img src="{{ asset(config('dir.methods').$method->image) }}" width="40px" />@endif</td>
-                                    <td>
-                                        <a href="{{route('admin.settings.methods.show',$method->id)}}" class="badge fs-12 @if($method->status === 'active') badge-outline-success text-success @else badge-outline-danger text-danger @endif" style="cursor: pointer">
-                                            {{ucfirst($method->status)}}
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <form action="{{ route('admin.settings.methods.destroy', $method->id) }}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <a type="button" href="{{ route('admin.settings.methods.edit', $method->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
-                                            <button type="submit" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
-                                        </form>
-                                    </td>
-                                </tr>
-                                @empty
-                                <tr>
-                                    <td colspan="5" class="text-center">No Methods found</td>
-                                </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div>
+                <div class="text-end">
+                    <a href="{{ route('admin.settings.methods.create') }}" class="btn btn-outline-success btn-sm mb-3"><i class="fa fa-plus"></i> Add Method</a>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Image</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($methods as $method)
+                            <tr>
+                                <td>{{ $method->name }}</td>
+                                <td>{{ $method->address }}</td>
+                                <td>@if($method->image)<img src="{{ asset(config('dir.methods').$method->image) }}" width="40px" />@endif</td>
+                                <td>
+                                    <a href="{{route('admin.settings.methods.show',$method->id)}}" class="badge fs-12 @if($method->status === 'active') badge-outline-success text-success @else badge-outline-danger text-danger @endif" style="cursor: pointer">
+                                        {{ucfirst($method->status)}}
+                                    </a>
+                                </td>
+                                <td>
+                                    <form action="{{ route('admin.settings.methods.destroy', $method->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <a type="button" href="{{ route('admin.settings.methods.edit', $method->id) }}" class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></a>
+                                        <button type="submit" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <td colspan="5" class="text-center">No Methods found</td>
+                            </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
