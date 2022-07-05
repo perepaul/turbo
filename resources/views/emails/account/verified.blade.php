5e21b8f1c8b1198a98d111e22829c9546887bc26
= <=>
2022-07-05 13:02:09 +0100

    typos

--- resources/views/emails/account/verified.blade.php
+++ resources/views/emails/account/verified.blade.php
@@ -4,7 +4,7 @@
 Your Documents have been reviewed and your account has been verified.
 
 
-Login access your customised dashboard where you can make a deposit, subscribe to a plan and commence trading.
+Login, access your customised dashboard where you can make a deposit, subscribe to a plan and commence trading.
 
 
 
