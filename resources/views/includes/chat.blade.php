a9c505fdf5e03d4a92a26ee829fa67583f3fb19e
= <=>
2022-07-12 09:22:04 +0100

    used brand icons instead

--- resources/views/includes/chat.blade.php
+++ resources/views/includes/chat.blade.php
@@ -2,7 +2,7 @@
 @if (request()->isUser() || request()->isFront())
 @if(isset($contact) && !is_null($contact->phone) && $contact->whatsapp == 'active')
 <a href="https://api.whatsapp.com/send?phone={{$contact?->phone}}" class="whatsapp-button-wrapper" target="_blank">
-    <i class="fa fa-whatsapp"></i>
+    <i class="fab fa-whatsapp"></i>
 </a>
 @endif
 @if(!empty($contact->chat_script))
