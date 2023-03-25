f76d851ea2406c60c592a7e099a06e18bb937faf
= <perepaul15@gmail.com>
2023-03-25 21:01:54 +0100

    make it optional

--- app/Http/Controllers/User/WithdrawController.php
+++ app/Http/Controllers/User/WithdrawController.php
@@ -72,7 +72,7 @@ class WithdrawController extends Controller
         } catch (\Throwable $th) {
             DB::rollBack();
             session()->flash('error', 'Failed to initiate withdrawal');
-            Log::error("$th->getMessage() file: $th->getFile() on line: $th->getLine()");
+            Log::error("$th?->getMessage()  file: $th->getFile() on line: $th->getLine()");
         }
         return redirect()->back();
     }
