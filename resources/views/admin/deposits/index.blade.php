02c69cb5c09545208cd099cebe7cd46aae500088
= <perepaul15@gmail.com>
2023-04-03 20:31:56 +0100

    null safty implementation on the deposit page

--- resources/views/admin/deposits/index.blade.php
+++ resources/views/admin/deposits/index.blade.php
@@ -20,16 +20,16 @@
                     @forelse ($deposits as $deposit)
                     <tr>
                         <td>{{ $deposit->reference }}</td>
-                        <td class="wspace-no">{{ $deposit->created_at->toDateString() }}</td>
-                        <td>{{ $deposit->user->name }}</td>
-                        <td>{{ $deposit->method->name }}</td>
-                        <td class="text-ov">{{ $deposit->address }}</td>
-                        <td>{{ format_money($deposit->amount, $deposit->user->currency->symbol) }}</td>
+                        <td class="wspace-no">{{ $deposit?->created_at?->toDateString() }}</td>
+                        <td>{{ $deposit?->user?->name }}</td>
+                        <td>{{ $deposit?->method?->name }}</td>
+                        <td class="text-ov">{{ $deposit?->address }}</td>
+                        <td>{{ format_money($deposit?->amount, $deposit?->user?->currency?->symbol) }}</td>
                         <td>
-                            <a href="{{ asset(config('dir.deposits').$deposit->proof) }}" target="_blank" class="btn btn-primary"><i class="fa fa-eye"></i></a>
-                            @if ($deposit->status == 'pending')
-                            <a href="{{ route('admin.deposits.approve', $deposit->id) }}" class="btn btn-success"><i class="fa fa-check"></i></a>
-                            <a href="{{ route('admin.deposits.decline', $deposit->id) }}" class="btn btn-danger"><i class="fa fa-times"></i></a>
+                            <a href="{{ asset(config('dir.deposits').$deposit?->proof) }}" target="_blank" class="btn btn-primary"><i class="fa fa-eye"></i></a>
+                            @if ($deposit?->status == 'pending')
+                            <a href="{{ route('admin.deposits.approve', $deposit?->id) }}" class="btn btn-success"><i class="fa fa-check"></i></a>
+                            <a href="{{ route('admin.deposits.decline', $deposit?->id) }}" class="btn btn-danger"><i class="fa fa-times"></i></a>
                             @endif
                         </td>
                     </tr>
