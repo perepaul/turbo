9b368d7ae3af3547d9acc7f51284eec493ab9b49
= <perepaul15@gmail.com>
2023-04-05 00:34:37 +0100

    fixing the trades section

--- resources/views/admin/trades/edit.blade.php
+++ resources/views/admin/trades/edit.blade.php
@@ -6,35 +6,35 @@
         <div class="card">
             <div class="card-body d-flex justify-content-center">
                 <div class="col-md-8 col-sm-12">
-                    <form action="{{route('admin.trades.update',$trade->id)}}" method="post">
+                    <form action="{{route('admin.trades.update',$trade?->id)}}" method="post">
                         @csrf
                         <div class="form-group">
                             <label for="">Name</label>
-                            <input type="text" class="form-control" value="{{$trade->user->name}}" readonly disabled>
+                            <input type="text" class="form-control" value="{{$trade?->user?->name}}" readonly disabled>
                         </div>
                         <div class="form-group">
                             <label for="">Currency</label>
-                            <input type="text" class="form-control" value="{{$trade->trade_currency->name}}" readonly disabled>
+                            <input type="text" class="form-control" value="{{$trade?->trade_currency?->name}}" readonly disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="">Amount</label>
-                            <input type="text" class="form-control" value="{{format_money($trade->amount,$trade->user->currency->symbol)}}" readonly disabled>
+                            <input type="text" class="form-control" value="{{format_money($trade?->amount,$trade?->user?->currency?->symbol)}}" readonly disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="">Type</label>
-                            <input type="text" class="form-control" value="{{ucfirst($trade->type)}}" readonly disabled>
+                            <input type="text" class="form-control" value="{{ucfirst($trade?->type)}}" readonly disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="">Demo Trade?</label>
-                            <input type="text" class="form-control" value="{{ucfirst($trade->is_demo)}}" readonly disabled>
+                            <input type="text" class="form-control" value="{{ucfirst($trade?->is_demo)}}" readonly disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="">Profit/Loss</label>
-                            <input type="text" class="form-control" value="{{old('profit') ?? $trade->profit}}" name="profit">
+                            <input type="text" class="form-control" value="{{old('profit') ?? $trade?->profit}}" name="profit">
                             <x-error :key="'profit'" />
                         </div>
                         <div class="text-center mt-5">
