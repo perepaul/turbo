b082ca633e46b9f8d6df347b4b68315328e57213
= <perepaul15@gmail.com>
2023-06-17 05:50:27 +0100

    styles updated

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -54,7 +54,7 @@
         <script src="/assets/back/vendor/global/global.min.js"></script>
         <script src="/assets/back/js/custom.min.js"></script>
         <script src="/assets/back/js/deznav-init.js"></script>
-        <script src="/assets/back/js/styleSwitcher.js"></script>
+        {{-- <script src="/assets/back/js/styleSwitcher.js"></script> --}}
         @include('includes.chat')
         @stack('js')
     </body>
